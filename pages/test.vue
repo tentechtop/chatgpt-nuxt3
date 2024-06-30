df895ad44bdd0d7694c66f5e58a17e4de2ad68be
redstar <1062467713@qq.com>
2024-06-30 23:39:16 +0800

    add:服务端获取数据

--- pages/test.vue
+++ pages/test.vue
@@ -2,9 +2,25 @@
   <div class="home-page">
 
 
-    <div :style="{ width: '100%' }">
-      <home-img-banner :banner-list="bannerData"></home-img-banner>
-    </div>
+
+    <adaptive-containers>
+
+      <div class="margin-box"/>
+      <div class="margin-box"/>
+
+      <div class="logo-container">
+        <img src="https://file.tentech.top/logo/tentech-logo.png">
+        <p>TENTECH</p>
+      </div>
+
+
+
+
+
+
+
+
+    </adaptive-containers>
 
 
 
@@ -26,135 +42,12 @@ import Breadcrumb from "~/components/breadcrumb.vue";
 
 
 
-const bannerData = ref([
-  {
-    title1: "测试",
-    title2: "",
-    title3: "",
-    title4: "",
-    title5: "",
-    title6: "",
-    mediaType:"img",
-    imgUrl: "https://tentech.oss-cn-shenzhen.aliyuncs.com/images/home/banner/banner-03.jpg?x-oss-process=image/resize,limit_0,m_fill,w_2280,h_880/quality,q_100",
-    videoPoster:"",
-    videoUrl:"",
-    buttonTitle:"测试",
-    buttonPath: "/",
-    alt:"测试",
-  },
-  {
-    title1: "牛逼",
-    title2: "",
-    title3: "",
-    title4: "",
-    title5: "",
-    title6: "",
-    mediaType:"img",
-    imgUrl: "https://support.colorlightinside.com/uploads/XR-bg_1656749996.png",
-    videoPoster:"https://kwun.oss-cn-shenzhen.aliyuncs.com/kwunphi4/videos/home/kwunphi.mp4?x-oss-process=video/snapshot,t_1000,m_fast",
-    videoUrl:"https://kwun.oss-cn-shenzhen.aliyuncs.com/kwunphi4/videos/home/kwunphi.mp4",
-    buttonTitle:"测试",
-    buttonPath: "/",
-    alt:"测试",
-  },
-  {
-    title1: "测试",
-    title2: "",
-    title3: "",
-    title4: "",
-    title5: "",
-    title6: "",
-    mediaType:"img",
-    imgUrl: "https://support.colorlightinside.com/uploads/-1818325876_1702533913.png",
-    videoPoster:"",
-    videoUrl:"",
-    buttonTitle:"测试",
-    buttonPath: "/",
-    alt:"测试",
-  },
 
 
-])
 
-const homeServeList = ref([
-  {
-    title1: t('customer.chat'),
-    title2: "",
-    title3: "",
-    title4: "",
-    title5: "",
-    title6: "",
-    mediaType:"img",
-    imgUrl: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/book-alt-dark-1?resMode=sharp2&op_usm=1.5,0.65,15,0&wid=48&hei=48&qlt=95&fmt=png-alpha&fit=constrain",
-    videoPoster:"",
-    videoUrl:"",
-    buttonTitle:"测试"+" >",
-    buttonPath: "/",
-    alt:"测试",
-  },
-  {
-    title1:   t('customer.chat'),
-    title2: "",
-    title3: "",
-    title4: "",
-    title5: "",
-    title6: "",
-    mediaType:"img",
-    imgUrl: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/resources-alt-dark-1?resMode=sharp2&op_usm=1.5,0.65,15,0&wid=48&hei=48&qlt=95&fmt=png-alpha&fit=constrain",
-    videoPoster:"",
-    videoUrl:"",
-    buttonTitle:"测试"+" >",
-    buttonPath: "/",
-    alt:"测试",
-  },
-  {
-    title1: "测试",
-    title2: "",
-    title3: "",
-    title4: "",
-    title5: "",
-    title6: "",
-    mediaType:"img",
-    imgUrl: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/blog-alt-dark?resMode=sharp2&op_usm=1.5,0.65,15,0&wid=48&hei=48&qlt=95&fmt=png-alpha&fit=constrain",
-    videoPoster:"",
-    videoUrl:"",
-    buttonTitle:"测试"+" >",
-    buttonPath: "/",
-    alt:"测试",
-  },
 
-])
 
-// @ts-ignore
-function getTargetDomain(language) {
-  let domain
-  // @ts-ignore
-  if (locales && locales.length) {
-    let defaultDomain
-    // @ts-ignore
-    for (let index = 0; index < locales.length; index++) {
-      // @ts-ignore
-      let item = locales[index]
-      if (item.code === language) {
-        domain = item.domain
-        if (domain) {
-          break;
-        }
-      } else if (item.code === 'cn') {
-        defaultDomain = item.code
-      } else if (!defaultDomain && item.code.startsWith('cn')) {
-        defaultDomain = item.code
-      }
-    }
-    if (!domain && defaultDomain) {
-      domain = defaultDomain
-    }
-    if (domain) {
-      return domain
-    }
-  }
-  return switchLocalePath(language)
-}
+
 
 useHead({
   title: t('seo.pages.home.title'),
@@ -172,6 +65,22 @@ useHead({
   background-color: #F9F9F9;
 }
 
+.logo-container{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
 
 
+}
+.logo-container >img{
+  width: 100px;
+  margin-right: 20px;
+}
+.logo-container >p{
+  font-size: 80px;
+  font-weight: 700;
+  color: var(--color-text-1);
+}
 </style>
